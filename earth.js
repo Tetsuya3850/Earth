29aa8c33f3902d91f19089f30ec76c5266dd77fb
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2017-06-17 18:18:06 +0900

    added orbit control

--- earth.js
+++ earth.js
@@ -1,61 +1,66 @@
-var scene, camera, light, renderer, controls;
+var scene, camera, light, renderer, controls, stats;
 var earthGeo, earthMat, loader, earthMesh;
 var WIDTH = window.innerWidth - 30,
     HEIGHT = window.innerHeight - 30;
 var container = document.getElementById('container');
 
-init();
-render();
-
 function init() {
   scene = new THREE.Scene();
 
+  camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 3000);
+
   renderer = new THREE.WebGLRenderer({antialiasing : true});
   renderer.setSize(WIDTH, HEIGHT);
-  container.appendChild(renderer.domElement);
-
-  camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 3000);
-  camera.position.set(0, 0, 0);
-
-  controls = new THREE.OrbitControls( camera, renderer.domElement );
-  controls.addEventListener( 'change', render );
 
   earthGeo = new THREE.SphereGeometry (30, 40, 400)
-  earthMat = new THREE.MeshPhongMaterial();
+  earthMat = new THREE.MeshNormalMaterial();
   earthMesh = new THREE.Mesh(earthGeo, earthMat);
   earthMesh.position.set(-100, 0, 0);
   earthMesh.rotation.y=3.7;
   earthMesh.rotation.z=0.5;
   scene.add(earthMesh);
 
-  loader = new THREE.TextureLoader();
-  loader.load('images/earthmap1k.jpg',
-    // Function when resource is loaded
-  	function ( texture ) {
-  		// do something with the texture
-  		earthMat.map = texture
-  	},
-  	// Function called when download progresses
-  	function ( xhr ) {
-  		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-  	},
-  	// Function called when download errors
-  	function ( xhr ) {
-  		console.log( 'An error happened' );
-  	});
-
+  camera.position.set(0, 0, 0);
   camera.lookAt( earthMesh.position );
 
-  light = new THREE.DirectionalLight(0xFFFFFF, 1, 0, Math.PI / 2, 1);
-  light.position.set(4000, 4000, 1500);
-  light.target.position.set (1000, 3800, 1000);
-  scene.add(light);
+  controls = new THREE.OrbitControls( camera );
 
+  
+
+  addStatsObject();
+
+  container.appendChild(renderer.domElement);
+
+  render();
+}
+
+function addStatsObject() {
+    stats = new Stats();
+    stats.setMode(0);
+
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '0px';
+    stats.domElement.style.top = '0px';
+
+    document.body.appendChild( stats.domElement );
 }
 
 function render() {
+  stats.update();
+  controls.update();
   renderer.render( scene, camera );
+  requestAnimationFrame(render);
 }
+
+function handleResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.onload = init;
+
+window.addEventListener('resize', handleResize, false);
 
 
 
